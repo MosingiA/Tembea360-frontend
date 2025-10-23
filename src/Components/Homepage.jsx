099dab63df7ed2f53916220e3371229a71d64c95
Tembea360 <tembea360@example.com>
2025-10-23 12:28:26 +0300

    Add top destinations section with local and international options

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -6,6 +6,7 @@ import ServicesSection from '../components/ServicesSection';
 import FeaturedProfessionals from '../components/FeaturedProfessionals';
 import TestimonialsSection from '../components/TestimonialsSection';
 import HowEventConnectWorks from '../components/HowEventConnectWorks';
+import TopDestinations from '../components/TopDestinations';
 import Footer from '../components/Footer';
 
 const Homepage = () => {
@@ -15,6 +16,7 @@ const Homepage = () => {
     <div className={`min-h-screen transition-colors duration-300 ${isDark ? 'bg-gray-900' : 'bg-white'}`}>
       <HeroSection />
       <FeaturedProfessionals />
+      <TopDestinations />
       <ServicesSection />
       <SubscriptionPlans />
       <TestimonialsSection />
